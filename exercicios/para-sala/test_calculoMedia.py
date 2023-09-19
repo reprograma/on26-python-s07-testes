2843556029afc425cc42e685e6b15b52accfef89
Jessica <jessicacristina.recife@gmail.com>
2023-09-18 21:56:02 -0300

    exercicio em sala

--- exercicios/para-sala/test_calculoMedia.py
+++ exercicios/para-sala/test_calculoMedia.py
@@ -1,6 +1,12 @@
 import unittest
 from calculo_media import calcular_media
 
+
+#Cenários de Teste
+#1. uma lista composta por zeros - resultado zero
+#2. uma lista vazia - resultado zero
+
+
 class TestCalcularMedia(unittest.TestCase):
     def test_media_lista_vazia(self):
         resultado = calcular_media([])
@@ -10,3 +16,4 @@ class TestCalcularMedia(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
