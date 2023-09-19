a80e6f073782521e806e39a533e1ddbc09924467
ThaysaLima <thayprl01@gmail.com>
2023-09-18 21:33:01 -0300

    Notas sobre cenários de teste que imaginamos

---
+++ exercicios/para-casa/notas_media_calculo.py
@@ -0,0 +1,15 @@
+#TESTE QUE IMAGINAMOS QUE PODEMOS FAZER
+
+#se todas as notas forem zero
+#so notas positivas
+#se tiver notas negativas
+#se só tiver notas negativas
+#ver o tamanho do vetor e esse vai ser o divisão
+#ver float 
+#ver se só tem 1 valor
+
+
+#o calcula da media aritmética é a a soma das notas dividida pela quantidade de notas: 
+    #ex: notas [3,9,8,7] e a divisão seria para 4
+
+#criar uma condição que se tiver so 1 valor na lista, ele não divide e apenas coloca o valor da lista.
