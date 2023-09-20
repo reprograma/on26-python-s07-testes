af1588e0caeebcac09230325ef9ec3afc8d2b89d
CaroliisRibeiro <anacalinda@gmail.com>
2023-09-20 11:59:28 -0300

    Atividade-casa-Calcular-Media

---
+++ exercicios/para-casa/calculo_media.py
@@ -0,0 +1,16 @@
+"""def calcular_media(numeros):
+    if not numeros:
+        return None 
+    return sum (numeros)/ len(numeros)
+"""
+
+def calcular_media(numeros):
+    numeros_validos = [x for x in numeros if isinstance(x, (int, float))]
+    if not numeros_validos:
+        return None
+    return sum(numeros_validos) / len(numeros_validos)
+
+
+    
+    
+    
