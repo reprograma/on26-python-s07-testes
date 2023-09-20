c3f420aa2b7e703a627a9ba0320019ecebd7a10d
Jessica <jessicacristina.recife@gmail.com>
2023-09-20 19:43:26 -0300

    exercicio 2 casa

--- exercicios/para-casa/media.py
+++ exercicios/para-casa/media.py
@@ -1,6 +1,24 @@
 #Teste - Média Aritmética
 
 def media_2 (x, y):
+
+    """media x e y
+
+    >>> media_2(10, 6.5)
+    8.25
+
+    >>> media_2('15', 15)
+    Traceback (most recent call last):
+    ...
+    AssertionError:  "x" precisa ser int ou float
+    """
+    
     assert isinstance (x, (int, float)), ' "x" precisa ser int ou float'
     assert isinstance (y, (int, float)), ' "y" precisa ser int ou float'
     return (x + y)/2
+
+
+# teste usando doctest
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
