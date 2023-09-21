15bd9b96b5ff68d6efac083c5cfc69557768e2fb
na-beatriz <anabealmeidas@gmail.com>
2023-09-20 22:15:37 -0300

    Exercício da Semana

--- exercicios/para-casa/test_calculoMedia.py
+++ exercicios/para-casa/test_calculoMedia.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
 #Teste lista com valores somente positivos:
 class TestCalcularMedia(unittest.TestCase):
     def test_media_lista_positiva(self):
-        resultado = calcular_media([3,2])
-        self.assertEqual(float(resultado))
+        resultado = calcular_media([3,2,7,12])
+        self.assertEqual(float(resultado, 6.0))
 
 if __name__ == '__main__':
     unittest.main()
@@ -22,8 +22,8 @@ if __name__ == '__main__':
 #Teste lista com valores somente negativos:
 class TestCalcularMedia(unittest.TestCase):
     def test_media_lista_negativa(self):
-        resultado = calcular_media([-10,-4])
-        self.assertEqual(float(resultado))
+        resultado = calcular_media([-10,-4, -10, -29,-5])
+        self.assertEqual(float(resultado, 11.6))
 
 if __name__ == '__main__':
     unittest.main()
@@ -31,7 +31,7 @@ if __name__ == '__main__':
 #Teste lista com valores positivos e negativos:
 class TestCalcularMedia(unittest.TestCase):
     def test_media_lista_positiva_negativa(self):
-        resultado = calcular_media([7,-3])
+        resultado = calcular_media([7,-3, -8, 20])
         self.assertEqual(float(resultado))
 
 if __name__ == '__main__':
@@ -40,8 +40,8 @@ if __name__ == '__main__':
 #Teste lista com valores fracionados:
 class TestCalcularMedia(unittest.TestCase):
     def test_media_lista_fracionada(self):
-        resultado = calcular_media([(3/4), (9/2)])
-        self.assertEqual(float(resultado))
+        resultado = calcular_media([(3/4), (9/2), (20/3)])
+        self.assertEqual(float(resultado, 3.97))
 
 if __name__ == '__main__':
     unittest.main()
@@ -50,7 +50,7 @@ if __name__ == '__main__':
 class TestCalcularMedia(unittest.TestCase):
     def test_media_lista_menos_valores(self):
         resultado = calcular_media([2,7])
-        self.assertEqual(resultado, "Mínimo 3 valores")
+        self.assertEqual(resultado, print("Mínimo 3 valores"))
 
 if __name__ == '__main__':
     unittest.main()
