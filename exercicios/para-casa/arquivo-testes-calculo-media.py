e4936efbf1a2b5005018cc5b9ac3ac555c92a306
freitascarla <contatocarlafreitas@gmail.com>
2023-09-20 22:12:29 -0300

    update envio exercício calculo média/testes

--- exercicios/para-casa/arquivo-testes-calculo-media.py
+++ exercicios/para-casa/arquivo-testes-calculo-media.py
@@ -1,5 +1,4 @@
-# Testando cenários mapeados para o código de cálculo da média
-
+# Mapeando e testando cenários para o código de cálculo da média
 
 import unittest     # importando o framework
 
@@ -25,15 +24,24 @@ class TesteMedia(unittest.TestCase):     # criando a classe de testes
         self.assertAlmostEqual(resultado, 6.33, places=2)
 
 # 4. Lista notas positivas e negativas
+    def teste_notas_positivas_negativas(self):
+        resultado = calcular_media([-8, 2, -14, 5])
+        self.assertEqual(resultado, -3.75)
 
 # 5. Lista notas vazia
     def teste_lista_vazia(self):
         resultado = calcular_media([])
         self.assertEqual(resultado, None)
 
-# 6. Lista notas inválidas
+# 6. Lista notas com decimais
+    def teste_notas_decimais(self):
+        resultado = calcular_media([3.64, 8.31, 9.04])
+        self.assertAlmostEqual(resultado, 7.0, places=2)
 
-# 7. Lista notas com decimais
+# 7. Lista notas inválidas (não consegui fazer o tratamento do erro na função de teste)
+    def teste_notas_inválidas(self):
+        if calcular_media(["xxx", 5, "rr"]):
+            self.assertRaises(ValueError)
 
 
 if __name__ == '__main__':     # chamando e executando os testes
--- exercicios/para-casa/arquivo_calculo_media.py
+++ exercicios/para-casa/arquivo_calculo_media.py
@@ -1,7 +1,7 @@
 # Calculando a média de uma lista de números
 
 def calcular_media(numeros):               # definindo a função para cálculo da média
-    if not numeros:                        # se não tiver números, retorne 0
+    if not numeros:                        # se não tiver números, não tem o que retornar
         return None
     return sum(numeros) / len(numeros)     # se tiver números, some os números e divida pelo tamamho do vetor de números
 
